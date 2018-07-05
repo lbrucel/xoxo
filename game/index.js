417b1610474732b9a777c40e07816bd0eaf88bfc
lbrucel <bruce.ledbetter@gmail.com>
2018-07-05 15:11:26 -0400

    passing over to samer

--- game/index.js
+++ game/index.js
@@ -1,6 +1,29 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable'
+let board = Map()
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+const initialState = {
+  board,
+  turn: 'X',
 }
+
+function move(turn, position) {
+  console.log('in move function', position)
+  return { type: 'move', position: position }
+}
+
+export default function gameReducer(state = initialState, action) {
+  console.log('hi from our reducer')
+  let nextPlayer = 'X'
+  switch (action.type) {
+    case 'move':
+      board = board.setIn(action.position, state.turn)
+      if (state.turn === 'X') {
+        nextPlayer = 'O'
+      }
+      return { board, turn: nextPlayer }
+    default:
+      return state
+  }
+}
+
+export { move }
